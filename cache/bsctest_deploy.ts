33ec43723146abd65964af35b242c90606e94373
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-11-17 11:50:39 +0100

    deployed

--- cache/bsctest_deploy.ts
+++ cache/bsctest_deploy.ts
@@ -1,4 +1,4 @@
 
-    export const PNK Contract = "0xaE2fF209D2bdd5AbD9b792f7B0a51f9ABEbB617F"
-    export const PNK Token Contract = "0xe705F1765E9981a4e05fb0F4438BEe11558ec31A"
+    export const PNK Contract = "0x0bcab0Da73048A96FE6b475925541C0064d4ebbe"
+    export const PNK Token Contract = "0x2f5EAf4593b67F8e717aF78f0Ce616E619483B2D"
   
